c99ecf579cd60f92b134efdc4d05332520d83192
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-01 14:17:55 -0700

    updated

--- about.js
+++ about.js
@@ -14,25 +14,29 @@ let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
 
-const complimentList = document.querySelector('ul')
-const catImage = document.getElementsByTagName('img')
-//const catCompliments = document.querySelectorAll('li')
-const catComplimentList = Array.from(document.querySelectorAll('li'))
-//catCompliments = complimentList.children
-//complimentList.appendChild(catCompliments)
-//console.log(complimentList[i])
+// const complimentList = document.querySelector('ul')
+// const catImage = document.getElementsByTagName('img')
+// //const catCompliments = document.querySelectorAll('li')
+// const catComplimentList = Array.from(document.querySelectorAll('li'))
+// //catCompliments = complimentList.children
+// //complimentList.appendChild(catCompliments)
+// //console.log(complimentList[i])
 
-function picCompliment(event) {
-    event.preventDefault();
+// function picCompliment(event) {
+//     event.preventDefault();
 	
-	for( let i = 0; i > catComplimentList.length; i++) {
-		console.log(catComplimentList[i])
-		alert(catComplimentList)
-	}
-    // document.getElementById('image');
-	// document.getElementById('ul')
-	// alert(document.getElementById(li))
-}
-console.log(catComplimentList[i])
-//document.querySelector('img').addEventListener('mouseover', picCompliment)
-catImage.addEventListener('mouseover', picCompliment)
+// 	for( let i = 0; i > catComplimentList.length; i++) {
+// 		console.log(catComplimentList[i])
+// 		alert(catComplimentList)
+// 	}
+//     // document.getElementById('image');
+// 	// document.getElementById('ul')
+// 	// alert(document.getElementById(li))
+// }
+// console.log(catComplimentList[i])
+// //document.querySelector('img').addEventListener('mouseover', picCompliment)
+// catImage.addEventListener('mouseover', picCompliment)
+
+document.querySelector('img').addEventListener('mouseover', () => {
+	alert("You are such an awesome cool cat")
+})
